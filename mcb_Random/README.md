4a36c2a35a6a7fd86f74c1016ef2507dd3c19294
Michael Büchel <michael@mcbSolutions.at>
2013-11-08 01:18:19 +0100

    v0.1

--- mcb_Random/README.md
+++ mcb_Random/README.md
@@ -3,13 +3,14 @@ Random
 
 Released under the [MIT license](http://opensource.org/licenses/MIT). Copyright (c) 2013 mcbSolutions.at
 
-**Version** 0.0 alpha; Please report errors.
+**Version** 0.1 alpha; Please report errors.
 
 **Add's random text and/or image to your site**
 
 Installation
 =============================================================================
-Copy/save the plugin into `plugins` folder
+1. Copy/save the plugin into `plugins` folder
+2. Setup the $config['mcb_random'][] array; see description below how to do this
 
 index.html
 -----------------------------------------------------------------------------
@@ -22,34 +23,38 @@ Use Twig value `{{ random.txt1 }}`, `{{ random.txt2 }}`, `{{ random.txt1 }}` whe
 			<sup class="floatRight">{{ random.txt2 }}</sup>
 			{{ random.img }}
 		</p>
-	{% endif %}  
-	
+	{% endif %}
+
 ### Example 2
 
 	{% if is_front_page %}
 		<p>{{ random.txt1 }}{{ random.img }}</p>
-	{% else %} 
+	{% else %}
 		{{ random.txt2 }}
-	{% endif %}  
-	
+	{% endif %}
+
 
 config
 -----------------------------------------------------------------------------
-### mcb_random_img_path
+
+### mcb_random_img_path (optional)
 **string**  
-Full path where the used images are located.
+Full path where the used images are located.  
+Default is `$config['base_url'] . "/content/images/"`
 
 	$config['mcb_random_img_path'] = $config['base_url'] . "/content/images/";
-	
-### mcb_random_img_alt
+
+### mcb_random_img_alt (optional)
 **string**  
-Title and alternative text for images, when missing in configuration
- 
+Title and alternative text for images, when missing in configuration.  
+Default is ``.
+
 		$config['mcb_random_img_alt'] = "Screen";
+
 ### mcb_random
 **string**  
-Semikolon separated values for random  
-	
+Semikolon separated values for random
+
 		$config['mcb_random'][] = 'Text 1;Text 2;Image;Image title';
-		
+
 **Note:** You are able to add as many `$config['mcb_random'][] = ...` lines you like.
--- mcb_Random/mcb_random.php
+++ mcb_Random/mcb_random.php
@@ -5,48 +5,49 @@
  *
  * @package Pico
  * @subpackage mcb_Random
- * @version 0.0.0 alpha
+ * @version 0.1 alpha
  * @author mcbSolutions.at <dev@mcbsolutions.at>
 
  */
 class mcb_Random {
 
-   private $pluginValues;
-   
+   private $path;
+   private $alt;
+   private $random;
+
    public function config_loaded(&$settings)
    {
-      global $config;
-      
-      
-      if(!isset($config['mcb_random_img_alt']))
-         $config['mcb_random_img_alt'] = "";
-      
-      foreach ($config['mcb_random'] as &$value) {
+      $this->path   = isset($settings['mcb_random_img_path']) ? $settings['mcb_random_img_path'] : $settings['base_url'] . "/content/images";
+      $this->alt    = &$settings['mcb_random_img_alt'];
+      $this->random = &$settings['mcb_random'];
+
+      if(!isset($settings['mcb_random_img_alt']))
+         $settings['mcb_random_img_alt'] = "";
+
+      foreach ($this->random as &$value) {
          $value = split(";", $value);
-         
-         $alt = isset($value[3]) ? $value[3] : $config['mcb_random_img_alt'];
-         
-         $value = array('txt1' => @$value[0], 
-                        'txt2' => @$value[1], 
+
+         $alt = isset($value[3]) ? $value[3] : $this->alt;
+
+         $value = array('txt1' => @$value[0],
+                        'txt2' => @$value[1],
                         'img' => isset($value[2]) ?
-                        
-                                 ("<img src=\"".$config['mcb_random_img_path']."/".$value[2]."\"".
+
+                                 ("<img src=\"".$this->path.DIRECTORY_SEPARATOR.$value[2]."\"".
                                        ($alt!="" ? " alt=\"$alt\" title=\"$alt\"" : "").">")
                                  :"", );
       }
    }
-   
+
    public function before_render(&$twig_vars, &$twig)
    {
-      global $config;
-      $num =  rand (0, count($config['mcb_random'])-1);
-      $twig_vars['random'] = $config['mcb_random'][$num];
+      $num =  rand (0, count($this->random)-1);
+      $twig_vars['random'] = $this->random[$num];
    }
    /* debug
    public function after_render(&$output)
    {
-        global $config;
-        $output = $output . "<pre>".htmlentities(print_r($config['mcb_random'],1))."</pre>";
+      $output = $output . "<pre style=\"background-color:white;\">".htmlentities(print_r($this,1))."</pre>";
    }*/
 }
 
